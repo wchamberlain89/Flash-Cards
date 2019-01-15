82575684c7d71b66c2348dbd1430841371c302d6
David Monarrez and ward chamberlain <david@>
2019-01-15 09:42:11 -0800

    it will have jquery effects

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+
+  $(".fade-btn").click(function () {
+    $(".fade-in").fadeToggle();
+    $(".fade-out").fadeToggle();
+  });
+
+  $('.slide-up-btn').click(function(){
+    $(".slide-up").slideUp("slow");
+  });
+
+  $('.slide-down-btn').click(function(){
+    $(".slide-down").slideDown("fast", function(){
+      $(".slide-down").css({"background-color" :"skyblue"});
+    });
+  });
+
+  $('.slide-toggle-btn').click(function(){
+    $(".slide-toggle").slideToggle("fast");
+  });
+});
